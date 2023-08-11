3f5634e6d73f3fd2784625f2c407617345f44040
garancesan <garancesan@gmail.com>
2023-08-11 15:28:47 +0200

    Basic wagtail project

---
+++ config/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-n$d8__mj8@5@64j&tws)(so8!qk8^q5510s*=^w40f^j2m#45)"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
